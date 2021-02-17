d80513711f3b8a58fd6a499222dc0c80677545fa
Katharine Fernandes <70667492+katharinefernandes@users.noreply.github.com>
2021-02-17 03:02:52 -0300

    Tela de Cadastro
    - Visualização da senha
    - Verificação email e senha :v

--- Front-end/Cadastro/Index.js
+++ Front-end/Cadastro/Index.js
@@ -1,23 +1,232 @@
+	//Senha
 
-	// Visualização da Senha
+		var globalForce = "";
 
-		function View_Password() {
+		//Funções de verificação
 
-			var password = form.password;
+			//hasChars
 
-			var image = document.getElementById("viewpassword");
+				function hasChars(variable){
 
-			if(password.type == "password") {
+					if (variable.match(/[a-z]+/) && variable.length > 7) {
 
-				image.style.display = "none";
+						return true;
+
+					}else{
+
+						return false;
+
+					}
+
+				}
+
+			//hasSpecialChars
+
+				function hasSpecialChars(variable){
+
+					if (variable.match(/[@#$%&;*?!_+=-]/)) {
+
+						return true;
+
+					}else{
+
+						return false;
+
+					}
+
+				}
+
+			//hasNumbers
+
+				function hasNumbers(variable){
+
+					if (variable.match(/[0-9]/)) {
+
+						return true;
+
+					}else{
+
+						return false;
+
+					}
+
+				}
+
+		// Verificação da Força da senha
+
+			function passwordForce() {
+
+				var password = form.password.value;
+
+				var force = "";
+
+				//ifs
+				
+					if (hasChars(password) && hasSpecialChars(password)) {
+
+						if (hasNumbers(password)) {
+
+							force = "Forte";
+
+						}else{
+
+							force = "Média";
+
+						}
+
+					}else{
+
+						force = "Fraca";
+
+					}
+
+				globalForce = force;
+
+				//Console.log
+
+					console.log("\nSenha: " + password);
+
+					console.log("hasChars: " + hasChars(password));
+
+					console.log("chars: " + password.length);
+
+					console.log("hasSpecialChars: " + hasSpecialChars(password));
+
+					console.log("globalForce: " + globalForce);
+
+				showForce(force, password);
+			}
+
+		//Exibição da Mensagem de Força da Senha
+			
+			function showForce(force, password) {
+
+				var showForce = document.getElementById("showForce");
+
+				var content = document.getElementById("content");
+
+				var passwordMessage = document.getElementById("passwordMessage");
+
+				//Mensagem da Força da senha
+
+					//if
+
+						if (password != "") {
+
+							showForce.style.display = "inline";
+
+						}else{
+
+							showForce.style.display = "none";
+
+						}
+
+					//switch
+
+						switch(force){
+
+							case "Fraca":
+
+								content.innerHTML = "Fraca";
+
+								content.style.color = "red";
+
+								break;
+
+							case "Média":
+
+								content.innerHTML = "Média";
+
+								content.style.color = "#FFA500";
+
+								break;
+
+							case "Forte":
+
+								content.innerHTML = "Forte";
+
+								content.style.color = "#15831A";
+
+								break;
+
+							default:
+
+								content.innerHTML = "Erro";
+
+								content.style.color = "Purple";
+
+						}
+
+				//Mensagem de senha
+
+					if (force == "Fraca" && password != "") {
+
+						passwordMessage.style.display = "inline";
+
+					}else{
+
+						passwordMessage.style.display = "none";
+
+					}
+
+			}
+
+	//Confirmação de senha
+
+		function passwordVerification(){
+
+			var password = form.password.value;
+
+			var password_C = form.password_C.value;
+
+			var Password_C_Message = document.getElementById("Password_C_Message");
+
+			if (password != password_C) {
+
+				Password_C_Message.style.display = "inline";
+
+			}else{
+
+				Password_C_Message.style.display = "none";
+
+			}
+
+		}
+
+	//Ver Senha
+
+		function viewPassword(id,name){
+
+			var image = document.getElementById(id);
+
+			var password = (id == "view") ? form.password : form.password_C;
+
+			if (password.type == "password") {
+
+				image.src = "https://github.com/requestion-projeto/REQUESTION-PLATAFORMA/blob/main/Front-end_imagens/Esqueceu_Senha/eye_black.png?raw=true";
 
 				password.type = "text";
 
-			} else {
+			}else{
 
-				image.style.display = "inline";
+				image.src = "https://github.com/requestion-projeto/REQUESTION-PLATAFORMA/blob/main/Front-end_imagens/Esqueceu_Senha/eye_white.png?raw=true";
 
 				password.type = "password";
+
+			}
+
+		}
+
+	//Validação
+
+		function Validate() {
+
+			if (globalForce == "Fraca") {
+
+				return false;
+
+				document.getElementByName("password").focus();
+
 			}
 
 		}
