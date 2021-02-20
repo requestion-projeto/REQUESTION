29148e59710bd96433c4885de3ce282d1987210c
Katharine Fernandes <70667492+katharinefernandes@users.noreply.github.com>
2021-02-19 21:47:16 -0300

    Tela de Perguntas Frequentes (FAQ)

---
+++ Front-end/FAQ/Index.js
@@ -0,0 +1,52 @@
+// DropDown - Faq
+
+	//DropDown: 1
+	    function View_dropdown_1() {
+
+	    	document.getElementById("DropDown_1").classList.toggle("show");
+
+	    }
+
+	//DropDown: 2
+	    function View_dropdown_2() {
+
+	    	document.getElementById("DropDown_2").classList.toggle("show");
+
+	    }
+
+    //DropDown: 3
+	    function View_dropdown_3() {
+
+	    	document.getElementById("DropDown_3").classList.toggle("show");
+
+	    }
+
+	//DropDown: 4
+	    function View_dropdown_4() {
+
+	    	document.getElementById("DropDown_4").classList.toggle("show");
+
+	    }
+
+	//DropDown: 5
+	    function View_dropdown_5() {
+
+	    	document.getElementById("DropDown_5").classList.toggle("show");
+
+	    }
+
+	//DropDown: 6
+	    function View_dropdown_6() {
+
+	    	document.getElementById("DropDown_6").classList.toggle("show");
+
+	    }
+
+	//DropDown: 7
+	    function View_dropdown_7() {
+
+	    	document.getElementById("DropDown_7").classList.toggle("show");
+
+	    }
+
+//Fim
