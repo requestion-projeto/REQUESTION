b176201b9dedf3814d9020e9597eadc1150dcb9a
Larissa Souza <larissa2004.souza@gmail.com>
2021-03-19 19:04:51 -0300

    Update backup requestion.sql

--- Back-end/backup requestion.sql
+++ Back-end/backup requestion.sql
@@ -109,7 +109,7 @@ CREATE TABLE IF NOT EXISTS `prova` (
   CONSTRAINT `fk_usuario_prova` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Copiando dados para a tabela requestion.prova: ~1 rows (aproximadamente)
+-- Copiando dados para a tabela requestion.prova: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `prova` DISABLE KEYS */;
 INSERT INTO `prova` (`cod_prova`, `nome`, `quant_questoes`, `data_alteração`, `cod_usuario`) VALUES
 	(1, 'P1- BDD', 6, '2021-03-02', 'RJ123456789');
@@ -151,29 +151,52 @@ CREATE TABLE IF NOT EXISTS `questao` (
   `tipo` varchar(50) NOT NULL,
   `privacidade` varchar(200) NOT NULL,
   `data_alteração` date NOT NULL,
-  `cod_tema` int(11) NOT NULL,
   `cod_usuario` varchar(11) NOT NULL,
   PRIMARY KEY (`cod_questao`),
   KEY `fk_usuario_questao` (`cod_usuario`),
-  KEY `fk_tema_questao` (`cod_tema`),
-  CONSTRAINT `fk_tema_questao` FOREIGN KEY (`cod_tema`) REFERENCES `tema` (`cod_tema`),
   CONSTRAINT `fk_usuario_questao` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`cod_usuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 -- Copiando dados para a tabela requestion.questao: ~10 rows (aproximadamente)
 /*!40000 ALTER TABLE `questao` DISABLE KEYS */;
-INSERT INTO `questao` (`cod_questao`, `nome_questão`, `enunciado`, `resposta`, `tipo`, `privacidade`, `data_alteração`, `cod_tema`, `cod_usuario`) VALUES
-	(1, 'Questão 1_ P1', 'O que é SQL', 'Linguagem de Consulta Estruturada ou SQL, é a linguagem de pesquisa declarativa padrão para banco de dados relacional.', 'Discursiva', 'Público', '2016-01-01', 16, 'RJ123456789'),
-	(2, 'Questão 2_ P1', 'Qual a finalidade da linguagem SQL?', 'SQL é uma linguagem usada para executar comando em bancos de dados relacionais, isto é, baseado em tabelas.', 'Discursiva', 'Público', '2013-01-01', 16, 'RJ123456789'),
-	(3, 'Questão 3_ P1', 'Qual a finalidade da linguagem DML?', 'DML ou linguagem de manipulação de dados (Data Manipulation Language) é uma linguagem de manipulação de dados (Data Manipulation Language, DML) é a linguagem que permite aos usuários fazer o acesso a os dados ou manipulá-los, conforme modelo de dados apropriado.', 'Discursiva', 'Privado', '2014-01-01', 16, 'RJ123456789'),
-	(4, 'Questão 4_ P1', 'Qual a finalidade da linguagem DDL?', 'DML ou linguagem de definição de dados (Data Definition Language) é uma linguagem de computador usada para a definição de estruturas de dados.', 'Discursiva', 'Privado', '2015-01-01', 16, 'RJ123456789'),
-	(5, 'Questão 5_ P1', 'Qual é o comando utilizado para criar tabela?', 'CREATE TABLE é o comando para criação da tabela e deve ser seguida pelo nome que daremos à tabela.', 'Discursiva', 'Público', '2019-01-01', 16, 'RJ123456789'),
-	(6, 'Questão 6_ P1', 'Com base no material, o que é um relacionamento entre entidades?', 'Relacionamento é uma associação entre entidades que representam situações que acontecem. Ex: Uma entidade Aluno está relacionada com a entidade Disciplina, pois o aluno cursa disciplina. É expresso por uma construção verbal. ', 'Discursiva', 'Privado', '2021-03-08', 18, 'RJ123456789'),
-	(7, 'Questão 7_ P1', 'Nome do aluno pode ser considerado um atributos referencial?', '', 'Objetiva', 'Público', '2020-04-14', 18, 'RJ123456789'),
-	(8, 'Questão 8_ P1', 'Qual alternativa apresenta apenas atributos descritivos', '', 'Objetiva', 'Público', '2018-12-31', 18, 'RJ123456789'),
-	(9, 'Questão 9_ P1', 'Por definição uma entidade deve ser:', '', 'Objetiva', 'Privado', '2011-02-09', 18, 'RJ123456789'),
-	(10, 'Questão 10_ P1', 'Quais desses atributos da entidade Aluno pode ser usado como atributo CHAVE', '', 'Objetiva', 'Privado', '2010-10-12', 18, 'RJ123456789');
+INSERT INTO `questao` (`cod_questao`, `nome_questão`, `enunciado`, `resposta`, `tipo`, `privacidade`, `data_alteração`, `cod_usuario`) VALUES
+	(1, 'Questão 1_ P1', 'O que é SQL', 'Linguagem de Consulta Estruturada ou SQL, é a linguagem de pesquisa declarativa padrão para banco de dados relacional.', 'Discursiva', 'Público', '2016-01-01', 'RJ123456789'),
+	(2, 'Questão 2_ P1', 'Qual a finalidade da linguagem SQL?', 'SQL é uma linguagem usada para executar comando em bancos de dados relacionais, isto é, baseado em tabelas.', 'Discursiva', 'Público', '2013-01-01', 'RJ123456789'),
+	(3, 'Questão 3_ P1', 'Qual a finalidade da linguagem DML?', 'DML ou linguagem de manipulação de dados (Data Manipulation Language) é uma linguagem de manipulação de dados (Data Manipulation Language, DML) é a linguagem que permite aos usuários fazer o acesso a os dados ou manipulá-los, conforme modelo de dados apropriado.', 'Discursiva', 'Privado', '2014-01-01', 'RJ123456789'),
+	(4, 'Questão 4_ P1', 'Qual a finalidade da linguagem DDL?', 'DML ou linguagem de definição de dados (Data Definition Language) é uma linguagem de computador usada para a definição de estruturas de dados.', 'Discursiva', 'Privado', '2015-01-01', 'RJ123456789'),
+	(5, 'Questão 5_ P1', 'Qual é o comando utilizado para criar tabela?', 'CREATE TABLE é o comando para criação da tabela e deve ser seguida pelo nome que daremos à tabela.', 'Discursiva', 'Público', '2019-01-01', 'RJ123456789'),
+	(6, 'Questão 6_ P1', 'Com base no material, o que é um relacionamento entre entidades?', 'Relacionamento é uma associação entre entidades que representam situações que acontecem. Ex: Uma entidade Aluno está relacionada com a entidade Disciplina, pois o aluno cursa disciplina. É expresso por uma construção verbal. ', 'Discursiva', 'Privado', '2021-03-08', 'RJ123456789'),
+	(7, 'Questão 7_ P1', 'Nome do aluno pode ser considerado um atributos referencial?', '', 'Objetiva', 'Público', '2020-04-14', 'RJ123456789'),
+	(8, 'Questão 8_ P1', 'Qual alternativa apresenta apenas atributos descritivos', '', 'Objetiva', 'Público', '2018-12-31', 'RJ123456789'),
+	(9, 'Questão 9_ P1', 'Por definição uma entidade deve ser:', '', 'Objetiva', 'Privado', '2011-02-09', 'RJ123456789'),
+	(10, 'Questão 10_ P1', 'Quais desses atributos da entidade Aluno pode ser usado como atributo CHAVE', '', 'Objetiva', 'Privado', '2010-10-12', 'RJ123456789');
 /*!40000 ALTER TABLE `questao` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela requestion.questao_tema
+DROP TABLE IF EXISTS `questao_tema`;
+CREATE TABLE IF NOT EXISTS `questao_tema` (
+  `cod_questao` int(11) NOT NULL,
+  `cod_tema` int(11) NOT NULL,
+  KEY `fk_questao_tema1` (`cod_questao`) USING BTREE,
+  KEY `fk_questao_tema` (`cod_tema`) USING BTREE,
+  CONSTRAINT `fk_questao_tema` FOREIGN KEY (`cod_tema`) REFERENCES `tema` (`cod_tema`),
+  CONSTRAINT `fk_questao_tema1` FOREIGN KEY (`cod_questao`) REFERENCES `questao` (`cod_questao`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+-- Copiando dados para a tabela requestion.questao_tema: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `questao_tema` DISABLE KEYS */;
+INSERT INTO `questao_tema` (`cod_questao`, `cod_tema`) VALUES
+	(1, 16),
+	(2, 16),
+	(3, 16),
+	(4, 16),
+	(5, 16),
+	(6, 18),
+	(7, 18),
+	(8, 18),
+	(9, 18),
+	(10, 18);
+/*!40000 ALTER TABLE `questao_tema` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela requestion.tema
 DROP TABLE IF EXISTS `tema`;
@@ -240,20 +263,20 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `cod_usuario` varchar(11) NOT NULL DEFAULT 'SP324400X',
   `nome` varchar(50) NOT NULL,
   `sobrenome` varchar(50) NOT NULL,
-  `e-mail` varchar(120) NOT NULL,
+  `email` varchar(120) NOT NULL,
   `senha` varchar(50) NOT NULL,
   `instituicao` varchar(200) NOT NULL,
   `grau_ensino` varchar(20) NOT NULL,
   `foto_perfil` longblob DEFAULT '0',
   `telefone` varchar(14) DEFAULT NULL,
-  `disciplina` varchar(2) DEFAULT NULL,
+  `disciplina` varchar(30) DEFAULT NULL,
   `UF` char(2) DEFAULT NULL,
   PRIMARY KEY (`cod_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Copiando dados para a tabela requestion.usuario: ~4 rows (aproximadamente)
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` (`cod_usuario`, `nome`, `sobrenome`, `e-mail`, `senha`, `instituicao`, `grau_ensino`, `foto_perfil`, `telefone`, `disciplina`, `UF`) VALUES
+INSERT INTO `usuario` (`cod_usuario`, `nome`, `sobrenome`, `email`, `senha`, `instituicao`, `grau_ensino`, `foto_perfil`, `telefone`, `disciplina`, `UF`) VALUES
 	('RJ123456789', 'Danilo', 'Viana Rodrigues', 'prof.daniloviana_puc-rio@hotmail.com.', 'Alalal22121', 'PUC-Rio', 'Superior', _binary 0x30, NULL, NULL, 'RJ'),
 	('RJ2304015X', 'Patrícia', 'Galindo Malta', 'patricia_galindomalta@hotmail.com.', 'Prof1212', 'UFRJ', 'Superior', _binary 0x30, NULL, NULL, 'RJ'),
 	('SP11254400X', 'Jaqueline', 'Souza Gravatto', 'profjaquelinegravatto@outlook.com', 'Jaque_ifsp2012', 'Instituto Federal de São Paulo (IFSP)', 'Médio', _binary 0x30, NULL, NULL, 'SP'),
